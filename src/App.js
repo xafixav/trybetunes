05ea42e871f3fe394f0002da3beafcd541043d73
xafixav <xafiworld@gmail.com>
2021-10-20 20:07:17 -0300

    requirement 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Album from './Components/Album';
+import Favorites from './Components/Favorites';
+import Login from './Components/Login';
+import NotFound from './Components/NotFound';
+import Profile from './Components/Profile';
+import ProfileEdit from './Components/ProfileEdit';
+import Search from './Components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>
-      <h1>let's goooo!</h1>);
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/*" component={ NotFound } />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/Components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <p>ola mundo sou o Login</p>
+      </div>
+    );
+  }
+}
