f84fa11054f3b29b2241e1349dbfe9e41b013983
xafixav <xafiworld@gmail.com>
2021-10-27 18:01:22 -0300

    requirement 13

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.MountProfile();
+  }
+
+  MountProfile = async () => {
+    this.setState({ isLoading: true });
+    const usuario = await getUser();
+    this.setState({ user: usuario, isLoading: false });
+  }
+
+  renderProfile = () => {
+    const { user } = this.state;
+    return (
+      <div>
+        <p>{user.name}</p>
+        <p>{user.name}</p>
+        <br />
+        <h1>Email:</h1>
+        <p>{user.email}</p>
+        <br />
+        <h1>Descrição:</h1>
+        <p>{user.description}</p>
+        <br />
+        <img src={ user.image } alt={ user.name } data-testid="profile-image" />
+        <br />
+        <Link to="/profile/edit"><span>Editar perfil</span></Link>
+      </div>
+    );
+  }
+
   render() {
+    const { isLoading } = this.state;
     return (
 
       <div data-testid="page-profile">
         <p>ola mundo sou o Profile</p>
+        {isLoading && <Loading />}
+        {this.renderProfile()}
       </div>
 
     );
