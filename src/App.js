c5081bcf606d71b3c69f967095afb1eed48fa540
OctavioLopes <octavio.hml@hotmail.com>
2022-10-05 14:00:34 -0300

    feat: Search input | button

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="background">
           <Header { ... this.state } />
           <Switch>
             <Route exact path="/" />
